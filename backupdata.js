adcea9b88a772a1d97eab07eadffc00812de33d6
novi <170887378+vianovi@users.noreply.github.com>
2024-06-12 06:40:41 +0700

    ppp

--- backupdata.js
+++ backupdata.js
@@ -1,28 +1,27 @@
-function backup() {
+
+function simpanSessionStorage() {
     try {
-        localStorage.setItem('backup_dataArray', JSON.stringify(backup_dataArray));
-        console.log('Data berhasil dibackup:', backup_dataArray);
-    }   catch (error) {
-        console.error('gagal membuat backup:', error);
+        sessionStorage.setItem('session_dataArray', JSON.stringify(backup_dataArray));
+        console.log('Data berhasil disimpan di sessionStorage:', backup_dataArray);
+    } catch (error) {
+        console.error('Gagal menyimpan data di sessionStorage:', error);
     }
 }
 
-function memuat_backup() {
+function muatSessionStorage() {
     try {
-        const storedData = localStorage.getItem('backup_dataArray');
+        const storedData = sessionStorage.getItem('session_dataArray');
         if (storedData) {
             backup_dataArray = JSON.parse(storedData);
             displayData_backup();
-            console.log('Data berhasil dimuat:', backup_dataArray);
+            console.log('Data berhasil dimuat dari sessionStorage:', backup_dataArray);
         } else {
-            console.log('Tidak ada data yang disimpan sebelumnya.');
+            console.log('Tidak ada data yang disimpan di sessionStorage.');
         }
     } catch (error) {
-        console.error('Gagal memuat data:', error);
+        console.error('Gagal memuat data dari sessionStorage:', error);
     }
 }
-
 function openIndexHtml() {
-    window.location.href = "index.html";
-    memuat_backup();
+    window.location.href = 'index.html';
 }
